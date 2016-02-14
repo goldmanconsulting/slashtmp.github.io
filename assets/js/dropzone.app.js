ae62b58bf99fa9db47626f2b54dd3e3a87a326b3
Grigori Goldman <grigori.goldman@gmail.com.au>
2016-02-14 23:20:03 +1100

    Changed the URL for the storage endpoint to be a subdomain of slashtmp.io instead of the S3 global bucket URL

--- assets/js/dropzone.app.js
+++ assets/js/dropzone.app.js
@@ -7,7 +7,7 @@ var options = {
     this.on("success", function(file, resp) {
       var elem = file.previewElement.querySelector(".btn-share");
       // TODO why is the resp only represents the body of the response, need the 'Location' HTTP Header attribute but can't get to it...
-      elem.setAttribute('data-dz-fileref', 'http://slashtmp.io.s3.amazonaws.com/' + file.key);
+      elem.setAttribute('data-dz-fileref', 'http://storage.slashtmp.io/' + file.key);
       elem.setAttribute('data-dz-filename', file.name);
     });
   },
@@ -24,8 +24,8 @@ var options = {
     formData.append('x-amz-algorithm', 'AWS4-HMAC-SHA256');
     formData.append('x-amz-storage-class', 'REDUCED_REDUNDANCY');
     formData.append('x-amz-credential', 'AKIAJBWJ4FGQSURTZVXQ/20160211/ap-southeast-2/s3/aws4_request');
-    formData.append('x-amz-signature', '78017f7fa1a4aa6e5e2125b84ab8b0d98d53a21d655d2f60db3ef5d0a3d62e76');
-    formData.append('policy', 'eyAiZXhwaXJhdGlvbiI6ICIyMTAwLTEyLTAxVDEyOjAwOjAwLjAwMFoiLAogICJjb25kaXRpb25zIjogWwogICAgeyJhY2wiOiAicHVibGljLXJlYWQifSwKICAgIHsiYnVja2V0IjogInNsYXNodG1wLmlvIn0sCiAgICB7IngtYW16LXN0b3JhZ2UtY2xhc3MiOiAiUkVEVUNFRF9SRURVTkRBTkNZIn0sCiAgICB7IngtYW16LWNyZWRlbnRpYWwiOiAiQUtJQUpCV0o0RkdRU1VSVFpWWFEvMjAxNjAyMTEvYXAtc291dGhlYXN0LTIvczMvYXdzNF9yZXF1ZXN0In0sCiAgICB7IngtYW16LWFsZ29yaXRobSI6ICJBV1M0LUhNQUMtU0hBMjU2In0sCiAgICB7IngtYW16LWRhdGUiOiAiMjAxNjAyMTFUMDA0MTMxWiJ9LAogICAgWyJzdGFydHMtd2l0aCIsICIka2V5IiwgIiJdLAogICAgWyJzdGFydHMtd2l0aCIsICIkY29udGVudC10eXBlIiwgIiJdLAogICAgWyJzdGFydHMtd2l0aCIsICIkY29udGVudC1kaXNwb3NpdGlvbiIsICIiXSwKICAgIFsiY29udGVudC1sZW5ndGgtcmFuZ2UiLCAwLCAxMDQ4NTc2MDBdCiAgXQp9');
+    formData.append('x-amz-signature', '6aee09143325b9365def661c4557b8683f6b5706cc87fd7e28cdb64763d3bbfc');
+    formData.append('policy', 'eyAiZXhwaXJhdGlvbiI6ICIyMTAwLTEyLTAxVDEyOjAwOjAwLjAwMFoiLAogICJjb25kaXRpb25zIjogWwogICAgeyJhY2wiOiAicHVibGljLXJlYWQifSwKICAgIHsiYnVja2V0IjogInN0b3JhZ2Uuc2xhc2h0bXAuaW8ifSwKICAgIHsieC1hbXotc3RvcmFnZS1jbGFzcyI6ICJSRURVQ0VEX1JFRFVOREFOQ1kifSwKICAgIHsieC1hbXotY3JlZGVudGlhbCI6ICJBS0lBSkJXSjRGR1FTVVJUWlZYUS8yMDE2MDIxMS9hcC1zb3V0aGVhc3QtMi9zMy9hd3M0X3JlcXVlc3QifSwKICAgIHsieC1hbXotYWxnb3JpdGhtIjogIkFXUzQtSE1BQy1TSEEyNTYifSwKICAgIHsieC1hbXotZGF0ZSI6ICIyMDE2MDIxMVQwMDQxMzFaIn0sCiAgICBbInN0YXJ0cy13aXRoIiwgIiRrZXkiLCAiIl0sCiAgICBbInN0YXJ0cy13aXRoIiwgIiRjb250ZW50LXR5cGUiLCAiIl0sCiAgICBbInN0YXJ0cy13aXRoIiwgIiRjb250ZW50LWRpc3Bvc2l0aW9uIiwgIiJdLAogICAgWyJjb250ZW50LWxlbmd0aC1yYW5nZSIsIDAsIDEwNDg1NzYwMF0KICBdCn0=');
     formData.append('Content-Type', file.type);
     formData.append('Content-Disposition', 'inline; filename=' + file.name);
   }
