298b007a994d6d4daf4fc72e9eb6f36d26a138fb
Grigori Goldman <grigori.goldman@gmail.com>
2017-05-19 21:08:52 +1000

    Improved handling of responses when sending emails

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,20 +1,36 @@
-function doSendEmail(email, fileref) {
+function doSendEmail(email, filename, fileref) {
   var url = 'https://fwhc9dx93f.execute-api.ap-northeast-1.amazonaws.com/latest/emails'
-  var data = JSON.stringify({email: email, fileref: fileref});
+  var data = JSON.stringify({email: email, filename: filename, fileref: fileref});
   $.ajax({
     type: 'POST',
     url: url,
     data: data,
     contentType: 'application/json',
-    success: function() { console.log('email sent') },
-    error: function() { console.log('email failed') }
+    success: onSendEmailSuccess,
+    error: onSendEmailError
   });
 }
 
 function sendEmail() {
+  toggleSendButton();
   var email = $('#share-link-via-email').val();
   var fileref = $('#share-link-fileref').val();
-  doSendEmail(email, fileref);
+  var filename = $('#share-link-filename').text();
+  doSendEmail(email, filename, fileref);
+}
+
+function toggleSendButton() {
+  var btn = $('.btn-send');
+  btn.prop('disabled', !btn.prop('disabled'));
+}
+
+function onSendEmailSuccess() {
+  toggleSendButton();
+  $('#share-link-dialog').modal('hide');
+}
+
+function onSendEmailError() {
+  toggleSendButton();
 }
 
 $(document).on('click', '.btn-send', sendEmail);
